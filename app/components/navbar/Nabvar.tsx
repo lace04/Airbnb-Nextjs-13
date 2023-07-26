5238389f865572ad6f1da4889b227d430857212f
lace04 <luisace04@gmail.com>
2023-07-25 20:31:31 -0500

    Navigation UI

---
+++ app/components/navbar/Nabvar.tsx
@@ -0,0 +1,22 @@
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
+
+function Nabvar() {
+  return (
+    <div className='fixed w-full bg-white z10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row justify-between items-center gap-3 md:gap-0'>
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Nabvar;
